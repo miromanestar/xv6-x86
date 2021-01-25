441b5997e6347096a8b00a51e74e7805bcfc3cf0
Miro Manestar <miroimanestar@gmail.com>
2021-01-25 08:50:32 -0500

    Did something of note...

--- fcntl.h
+++ fcntl.h
@@ -1,4 +1,4 @@
-#define O_RDONLY  0x000
-#define O_WRONLY  0x001
-#define O_RDWR    0x002
-#define O_CREATE  0x200
+#define O_RDONLY  0x000 // 0000 0000 0000
+#define O_WRONLY  0x001 // 0000 0000 0001
+#define O_RDWR    0x002 // 0000 0000 0010
+#define O_CREATE  0x200 // 0010 0000 0000
