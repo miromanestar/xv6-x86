3ba2d5019ba919f4b93da64bd83c09a43eb50a74
Miro Manestar <miroimanestar@gmail.com>
2021-02-19 11:23:49 -0500

    Worked on shell

--- ezsh.c
+++ ezsh.c
@@ -14,6 +14,7 @@
 struct cmd {
   char* c;
   char** argv;
+  int argc;
 };
 
 char whitespace[] = " \t\r\n\v";
@@ -29,19 +30,26 @@ int isSpace(const char c) {
 struct cmd* parse_cmd(const char* buf) {
     struct cmd* cmd;
     cmd = malloc(sizeof (struct cmd));
+    cmd->c = malloc(100 * sizeof (char));
+    cmd->argv = malloc(10 * sizeof (char*));
+    cmd->argv[0] = malloc(30 * sizeof (char));
 
     int arg = 0;
     for (int i = 0; buf[i]; i++) {
         if (isSpace(buf[i])) {
             arg++;
+            cmd->argv[arg] = malloc(30 * sizeof (char)); 
             continue;
         }
         if (arg == 0) {
             cmd->c[i] = buf[i];
+            continue;
         }
+
         cmd->argv[arg][i] = buf[i];
     }
 
+    cmd->argc = arg;
     return cmd;
 }
 
@@ -56,9 +64,11 @@ void ezsh_loop(void) {
         struct cmd* cmd = parse_cmd(buf);
         
         printf(2, cmd->c);
-        printf(2, cmd->argv[0]);
-        printf(2, cmd->argv[1]);
 
+        for (int i = 1; i < cmd->argc; i++)
+            printf(2, "%s\n", cmd->argv[i]);
+
+        //Free each individual item in struct
         //wait();
     }
 }
